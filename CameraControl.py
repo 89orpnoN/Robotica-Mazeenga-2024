622b1bc2c40ae369e7822e321eac23311c913b0d
89orpnon <9698022@itisrossi.vi.it>
2024-02-25 22:08:35 +0100

    Riconoscimento di colori

--- CameraControl.py
+++ CameraControl.py
@@ -1,15 +1,13 @@
 import cv2
 import Platform_Detection as PD
+import numpy as np
 def NewCapture(Cam):
     cap = PD.DoThisOnPlatform("Windows", cv2.VideoCapture, [Cam, cv2.CAP_DSHOW], cv2.VideoCapture, [Cam])[1]
     return cap
-def UpdateFrames(cap):
+def ShowCapture(cap):
     while True:
 
         Showframe(UpdateFrame(cap))
-        # Break the loop if 'q' key is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
 
     cap.release()
     cv2.destroyAllWindows()
@@ -25,3 +23,28 @@ def UpdateFrame(cap):
 
 def Showframe(frame):
     cv2.imshow("USB Camera", frame)
+
+def RangeMask(frame,color,tollerance):
+    ColorLow = np.array(color) - tollerance
+    ColorHigh = np.array(color) + tollerance
+    mask = cv2.inRange(frame, ColorLow, ColorHigh)
+    return mask
+
+def Contorni(mask, minArea = 0): #trova tutti i contorni che rispettano le condizioni
+    contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)[0]
+    valid_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > minArea]
+    return valid_contours
+def Contorno(mask,minArea = 0): #trova il contorno più grande
+
+    MaxArea = minArea
+    Contorno = None
+
+    Contorni = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)[0]
+
+    for i in Contorni:
+        area = cv2.contourArea(i)
+        if area > MaxArea:
+            MaxArea = area
+            Contorno = i
+
+    return Contorno
--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 import Platform_Detection as PD
 import threading
 import CameraControl as CC
+import cv2
+import numpy as np
 # Open the video capture object
 
 cap = CC.NewCapture(0)
+#visione videocamera
+#threading.Thread(target=CC.UpdateFrames,args=[cap]).start()
 
-threading.Thread(target=CC.UpdateFrames,args=[cap]).start()
+while True:
+    Frame = CC.UpdateFrame(cap)
+    yellow = [48, 131, 117]
+    tollerance = 25
+    mask = CC.RangeMask(Frame,yellow,tollerance)
 
+
+    min_contour_area = 600
+
+    contorni = CC.Contorni(mask,min_contour_area)
+    contorno = CC.Contorno(mask, min_contour_area)
+
+    cv2.drawContours(Frame, contorni, -1, (0, 255, 0), 3)
+
+
+    x, y, w, h = cv2.boundingRect(contorno)
+    cv2.rectangle(Frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    cv2.imshow('Colored Squares Detection', Frame)
+
+
+cv2.destroyAllWindows()
