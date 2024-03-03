be7dc6cbc8aa32fb8b0f1c0eb929d3e6645b9d84
89orpnon <9698022@itisrossi.vi.it>
2024-03-03 14:07:16 +0100

    p

--- Vl53l0x.py
+++ Vl53l0x.py
@@ -29,24 +29,24 @@ class Tof_Switch:
       self._address = new_addr
       self.VL53L0X.change_address(new_addr)
 
-    def StartRanging(self, ranges):
-      self.VL53L0X.open()
-      self.VL53L0X.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+  def StartRanging(self, ranges):
+    self.VL53L0X.open()
+    self.VL53L0X.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
 
-      timing = self.VL53L0X.get_timing()
-      if timing < 20000:
-          timing = 20000
-      print("Timing %d ms" % (timing / 1000))
+    timing = self.VL53L0X.get_timing()
+    if timing < 20000:
+      timing = 20000
+    print("Timing %d ms" % (timing / 1000))
 
-      for count in range(1, 101):
-          distance = self.VL53L0X.get_distance()
-          if distance > 0:
-              print("%d mm, %d cm, %d" % (distance, (distance / 10), count))
+    for count in range(1, 101):
+      distance = self.VL53L0X.get_distance()
+      if distance > 0:
+          print("%d mm, %d cm, %d" % (distance, (distance / 10), count))
 
-          time.sleep(timing / 1000000.00)
+      time.sleep(timing / 1000000.00)
 
-      self.VL53L0X.stop_ranging()
-      self.VL53L0X.close()
+    self.VL53L0X.stop_ranging()
+    self.VL53L0X.close()
 
 
 tof = Tof_Switch(1,0x29,11)
