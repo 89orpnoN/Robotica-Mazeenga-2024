9c433be527295cb6badaddbd334bd02a4839007c
89orpnon <9698022@itisrossi.vi.it>
2024-03-04 00:08:45 +0100

    p

--- Vl53l0x.py
+++ Vl53l0x.py
@@ -11,6 +11,9 @@ class Tof_Switch:
       self.Xshut = LED(pin)
       self.Off()
 
+      self._IsOpen = False
+      self._IsReady = False
+
       self._bus = bus
       self._address = addr
       self.VL53L0X = None
@@ -18,31 +21,36 @@ class Tof_Switch:
       self.Timing = None
 
   def Initialize(self, deactivate = True): #inizializza il sensore e poi lo spegne di default
-
       self.VL53L0X = VL53L0X.VL53L0X(i2c_bus=self._bus, i2c_address=self._address)
 
   def On(self): #attiva il sensore
       self.Xshut.on()
-      self.Activated = True
       wait()
+      self.Activated = True
 
   def Off(self): #disattiva il sensore
       self.Xshut.off()
-      self.Activated = False
       wait()
-
-  def IsOpen(self):
-      if self.VL53L0X != None and self.VL53L0X._dev != None: return True
-      return False
+      self.Activated = False
 
   def Open(self):
       self.VL53L0X.open()
 
+      self._IsOpen = True
 
+
+  def StartRange(self):
+      self.VL53L0X.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+      self.Timing = tof.VL53L0X.get_timing()
+
+      self._IsReady = True
 
   def Close(self):
       self.VL53L0X.stop_ranging()
       self.VL53L0X.close()
+
+      self._IsOpen = False
+      self._IsReady = False
 
 def ChangeAddress(tof,new_addr): #cambia l'indirizzo del/dei sensore/i
       tof._address = new_addr
@@ -58,31 +66,35 @@ def StartRanging(tof, ranges): # funzione di prova, poco utile in gara
 
 
 def Getrange(tof): # Ottiene un singolo range
-    if not tof.IsOpen():
-        tof.On()
-        wait()
-        tof.VL53L0X.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
-        tof.Timing = tof.VL53L0X.get_timing()
+    if not tof._IsOpen:
+        tof.Open()
+    if not tof._IsReady:
+        tof.StartRange()
 
     distance = tof.VL53L0X.get_distance()
     sleep(tof.Timing / 1000000.00) #si può fare meglio ma così va bene per ora
     return distance
 
 def Setup_Tofs(tofs): #cambia l'indirizzo dei tof in base al loro ordine nell'array
-    i = 0
+    i = 1
     for tof in tofs:
-        if not tof.IsOpen() and not tof.Activated:
+        if not tof._IsOpen and not tof.Activated:
+            if i != 0:
                 tof.Initialize(False)
+                tof.On()
+                ChangeAddress(tof, tof._address + 1)
                 tof.Open()
+                tof.Off()
+
         else:
             raise Exception("Classe Tof_Switch già inizializzata")
         i+=1
 
 
 
-tof = Tof_Switch(1,0x2B,17) #canale I2C, Indirizzo base del sensore, pin di disattivazione
+tof = Tof_Switch(1,0x29,17) #canale I2C, Indirizzo base del sensore, pin di disattivazione
 
-tof2 = Tof_Switch(1,0x2D,27)
+tof2 = Tof_Switch(1,0x29,27)
 
 Setup_Tofs([tof,tof2])
 
