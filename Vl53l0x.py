9eaad453649c90980bb2e2a5afa168e1d7b62849
89orpnon <9698022@itisrossi.vi.it>
2024-03-03 02:20:30 +0100

    p

--- Vl53l0x.py
+++ Vl53l0x.py
@@ -1,9 +1,20 @@
 import time
 import VL53L0X
+from gpiozero import LED
+from time import sleep
 
+
+Xshut = LED(11)
+Xshut2 = LED(13)
 # Create a VL53L0X object
+Xshut2.on()
+time.sleep(0.1)
 tof = VL53L0X.VL53L0X(i2c_bus=1,i2c_address=0x29)
-tof2 = VL53L0X.VL53L0X(i2c_bus=1,i2c_address=0x29)
+Xshut2.off()
+time.sleep(0.1)
+Xshut.on()
+tof2 = VL53L0X.VL53L0X(i2c_bus=1,i2c_address=0x30)
+Xshut.off()
 # I2C Address can change before tof.open()
 # tof.change_address(0x32)
 tof.open()
