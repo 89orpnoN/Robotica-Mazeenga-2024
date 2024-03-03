539da251db230c29424a75072b129b3ef51be85d
89orpnon <9698022@itisrossi.vi.it>
2024-03-03 13:41:20 +0100

    p

--- Vl53l0x.py
+++ Vl53l0x.py
@@ -2,36 +2,66 @@ import time
 import VL53L0X
 from gpiozero import LED
 from time import sleep
+def wait():
+    time.sleep(0.5)
+
+class Tof_Switch:
+
+  def __init__(self, bus,addr, pin):
+    self.Xshut = pin
+    self.VL53L0X = None
+    self._bus = bus
+    self._address = addr
+
+  def Initialize(self, deactivate = True):
+      self.Xshut = LED(self.Xshut)
+      self.Xshut.off()
+      self.VL53L0X = VL53L0X.VL53L0X(i2c_bus=self._bus, i2c_address=self._address)
+      self.VL53L0X.open()
+      if deactivate: self.open()
+  def open(self):
+      self.Xshut.off()
+
+  def close(self):
+      self.Xshut.on()
+
+  def ChangeAddress(self,new_addr):
+      self._address = new_addr
+      self.VL53L0X.change_address(new_addr)
+
+tof = Tof_Switch.Tof_Switch(1,0x29,11)
+tof2 = Tof_Switch.Tof_Switch(1,0x29,13)
+
+tof2.close()
+
+wait()
+
+tof.Initialize()
+
+wait()
+
+tof2.open()
+wait()
+tof2.Initialize()
 
 
-Xshut = LED(11)
-Xshut2 = LED(13)
-# Create a VL53L0X object
-Xshut2.on()
-time.sleep(0.1)
-tof = VL53L0X.VL53L0X(i2c_bus=1,i2c_address=0x29)
-Xshut2.off()
-time.sleep(0.1)
-Xshut.on()
-tof2 = VL53L0X.VL53L0X(i2c_bus=1,i2c_address=0x30)
-Xshut.off()
-# I2C Address can change before tof.open()
-# tof.change_address(0x32)
+wait()
 tof.open()
+wait()
 # Start ranging
 tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
 
-timing = tof.get_timing()
+timing = tof.VL53L0X.get_timing()
 if timing < 20000:
     timing = 20000
 print("Timing %d ms" % (timing/1000))
 
-for count in range(1, 301):
-    distance = tof.get_distance()
+for count in range(1, 101):
+    distance = tof.VL53L0X.get_distance()
     if distance > 0:
         print("%d mm, %d cm, %d" % (distance, (distance/10), count))
 
     time.sleep(timing/1000000.00)
 
-tof.stop_ranging()
-tof.close()
+tof.VL53L0X.stop_ranging()
+tof.VL53L0X.close()
