455023fba759cd9127faace6009d2bdbc02108d0
89orpnon <9698022@itisrossi.vi.it>
2024-03-03 19:03:01 +0100

    p

--- Vl53l0x.py
+++ Vl53l0x.py
@@ -15,13 +15,13 @@ class Tof_Switch:
       self._address = addr
       self.VL53L0X = None
 
+      self.Timing = None
+
   def Initialize(self, deactivate = True): #inizializza il sensore e poi lo spegne di default
       self.On()
 
 
       self.VL53L0X = VL53L0X.VL53L0X(i2c_bus=self._bus, i2c_address=self._address)
-      self.VL53L0X.open()
-      self.VL53L0X.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
       if deactivate: self.Off()
   def On(self): #attiva il sensore
       self.Xshut.on()
@@ -33,7 +33,19 @@ class Tof_Switch:
       self.Activated = False
       wait()
 
+  def IsOpen(self):
+      if self.VL53L0X._dev != None: return True
+      return False
 
+  def Open(self):
+      self.VL53L0X.open()
+      self.VL53L0X.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+      self.Timing = tof.VL53L0X.get_timing()
+
+
+  def Close(self):
+      self.VL53L0X.stop_ranging()
+      self.VL53L0X.close()
 
 def ChangeAddress(tof,new_addr): #cambia l'indirizzo del/dei sensore/i
       tof._address = new_addr
@@ -41,33 +53,23 @@ def ChangeAddress(tof,new_addr): #cambia l'indirizzo del/dei sensore/i
 
 
 def StartRanging(tof, ranges): # funzione di prova, poco utile in gara
-    tof.VL53L0X.open()
-    tof.VL53L0X.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
-
-    timing = tof.VL53L0X.get_timing()
-
-    if timing < 20000:
-      timing = 20000
-    print("Timing %d ms" % (timing / 1000))
+    if !tof.IsOpen():
+        tof.Open()
 
     for count in range(1, ranges):
-      distance = tof.VL53L0X.get_distance()
+      distance = Getrange(tof)
       if distance > 0:
           print("%d mm, %d cm, %d" % (distance, (distance / 10), count))
 
-      sleep(timing / 1000000.00)
 
-    tof.VL53L0X.stop_ranging()
-    tof.VL53L0X.close()
 
 def Getrange(tof): # funzione di prova, poco utile in gara
-
-    timing = tof.VL53L0X.get_timing()
+    if !tof.IsOpen():
+        tof.Open()
     distance = tof.VL53L0X.get_distance()
     sleep(timing / 1000000.00)
-    tof.VL53L0X.stop_ranging()
-    tof.VL53L0X.close()
     return distance
+
 
 
 tof = Tof_Switch(1,0x29,17)
