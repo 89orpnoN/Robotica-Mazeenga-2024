82f1b95f14c49c6b6baa173db5d8f53aecbfff8c
89orpnon <9698022@itisrossi.vi.it>
2024-03-03 15:05:27 +0100

    p

--- Vl53l0x.py
+++ Vl53l0x.py
@@ -9,44 +9,50 @@ class Tof_Switch:
 
   def __init__(self, bus,addr, pin):
     self.Xshut = pin
-    self.VL53L0X = None
+    self.Activated = None
+
     self._bus = bus
     self._address = addr
+    self.VL53L0X = None
 
   def Initialize(self, deactivate = True):
       self.Xshut = LED(self.Xshut)
-      self.Xshut.off()
-      self.VL53L0X = VL53L0X.VL53L0X(i2c_bus=self._bus, i2c_address=self._address)
-      if deactivate: self.open()
-  def open(self):
       self.Xshut.on()
+      self.Activated = False
+
+      self.VL53L0X = VL53L0X.VL53L0X(i2c_bus=self._bus, i2c_address=self._address)
+      if deactivate: self.On()
+  def On(self):
+      self.Xshut.on()
+      self.Activated = True
+
+  def Off(self):
       self.Xshut.off()
+      self.Activated = False
 
-  def close(self):
-      self.Xshut.off()
 
-  def ChangeAddress(self,new_addr):
-      self._address = new_addr
-      self.VL53L0X.change_address(new_addr)
+def ChangeAddress(tof,new_addr):
+      tof._address = new_addr
+      tof.VL53L0X.change_address(new_addr)
 
-  def StartRanging(self, ranges):
-    self.VL53L0X.open()
-    self.VL53L0X.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+def StartRanging(tof, ranges):
+    tof.VL53L0X.open()
+    tof.VL53L0X.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
 
-    timing = self.VL53L0X.get_timing()
+    timing = tof.VL53L0X.get_timing()
     if timing < 20000:
       timing = 20000
     print("Timing %d ms" % (timing / 1000))
 
     for count in range(1, ranges):
-      distance = self.VL53L0X.get_distance()
+      distance = tof.VL53L0X.get_distance()
       if distance > 0:
           print("%d mm, %d cm, %d" % (distance, (distance / 10), count))
 
       time.sleep(timing / 1000000.00)
 
-    self.VL53L0X.stop_ranging()
-    self.VL53L0X.close()
+    tof.VL53L0X.stop_ranging()
+    tof.VL53L0X.close()
 
 
 tof = Tof_Switch(1,0x29,11)
@@ -55,7 +61,7 @@ tof2 = Tof_Switch(1,0x29,13)
 
 
 tof.Initialize()
-tof.close()
+tof.Off()
 
 
 wait()
@@ -63,9 +69,9 @@ tof2.Initialize()
 
 
 wait()
-tof.open()
+tof.On()
 wait()
 
-tof.StartRanging(50)
+StartRanging(tof,50)
 
-tof2.StartRanging(50)
+StartRanging(tof2,50)
