c3fab4c145184c18cd96fe32be9a6108fd8e6ea0
89orpnon <9698022@itisrossi.vi.it>
2024-03-09 23:06:39 +0100

    p

--- Vl53l0x.py
+++ Vl53l0x.py
@@ -123,6 +123,7 @@ def Setup_Tofs(pins): #cambia l'indirizzo dei tof in base al loro ordine nell'ar
             wait()
             #controllo che non sia andato a puttane
             status = _TOF_LIBRARY.VL53L0X_GetPalState(tof.VL53L0X._dev)
+            print("lo stato è: " + str(status))
             if status < 90:
                 break
         i+=1
