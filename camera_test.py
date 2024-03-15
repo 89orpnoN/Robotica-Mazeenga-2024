1302be6b7716b88caa0000b95d64fde9c9b96711
BarbanAlberto <9698260@itisrossi.vi.it>
2024-03-15 09:20:45 +0100

    Ottimizzazioni

--- camera_test.py
+++ camera_test.py
@@ -34,42 +34,38 @@ def MonitorYellow(UpdatedFrames):
         
         PD.DoThisOnPlatform("Windows",CC.Showframe,[Frame,'Colored Squares Detection'])
 
-        print("colore: " + str((time.process_time_ns() - epoch) / 1000000000))
+        print("colore: " + str((time.process_time_ns() - epoch) / 1000000 + "ms"))
         epoch = time.process_time_ns()
 
 def ScanLetters(UpdatedFrames):
-    os.environ['TESSDATA_PREFIX'] = os.getcwd()+"/Tesseract OCR models"
-    epoch = time.process_time_ns()
+    #os.environ['TESSDATA_PREFIX'] = os.getcwd()+"/Tesseract OCR models"
     while True:
+        epoch = time.process_time_ns()
         Frame = UpdatedFrames.getLFrame()
         Frame=CC.ToBlackWhite(Frame,90)
-        print(pytesseract.image_to_string(Frame, config='--psm 10 --oem 0 -c tessedit_char_whitelist=HSUu',lang="ita"))
+        print(pytesseract.image_to_string(Frame, config='--psm 10 -c tessedit_char_whitelist=HSsUu').lower())
         PD.DoThisOnPlatform("Windows",CC.Showframe,[Frame,'Letter'],print,["Lettere sta andando"])
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
-        print("lettere: " + str((time.process_time_ns() - epoch) / 1000000000))
-        epoch = time.process_time_ns()
+        print("lettere: " + str((time.process_time_ns() - epoch) / 1000000 + "ms"))
 
 def startCamering():
-    cap = CC.NewCapture(-1)
+    cap = CC.NewCapture(0)
     cap.set(3, 160)
     cap.set(4, 120)
 
-
-    cap2 = CC.NewCapture(-1)
-
-
+    cap2 = CC.NewCapture(2)
     cap2.set(3, 160)
     cap2.set(4, 120)
 
     UpdatedFrames = CC.FrameCapture(cap)
     UpdatedFrames2 = CC.FrameCapture(cap2)
 
-    threading.Thread(target=MonitorYellow,args=[UpdatedFrames],name="UserThread").start()
+    #threading.Thread(target=MonitorYellow,args=[UpdatedFrames],name="UserThread").start()
     threading.Thread(target=ScanLetters,args=[UpdatedFrames],name="UserThread").start()
 
-    threading.Thread(target=MonitorYellow, args=[UpdatedFrames2], name="UserThread").start()
+    #threading.Thread(target=MonitorYellow, args=[UpdatedFrames2], name="UserThread").start()
     threading.Thread(target=ScanLetters, args=[UpdatedFrames2], name="UserThread").start()
 
     for t in threading.enumerate():
--- motors.py
+++ motors.py
@@ -4,13 +4,18 @@ from time import sleep as __sleep
 robot = __Robot(left=__Motor(5, 6), right=__Motor(12, 13))
 
 def test():
+  print("forward")
   robot.forward()
   __sleep(1)
+  print("backward")
   robot.backward()
   __sleep(1)
+  print("right")
   robot.right()
   __sleep(1)
+  print("left")
   robot.left()
   __sleep(1)
+  print("stop")
   robot.stop()
   __sleep(1)
