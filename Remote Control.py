d6dc326216aba16b4fff0f577160042c02e5b8c2
89orpnon <9698022@itisrossi.vi.it>
2024-03-15 21:42:43 +0100

    p

---
+++ Remote Control.py
@@ -0,0 +1,32 @@
+import Vl53l0x
+from victim import victim
+from motors import robot
+from gpiozero import Button
+from time import sleep
+import threading
+
+
+
+last_button = [""]
+before_last_button = [""]
+mvm = 0
+def checkbutton(last_button):
+    while True:
+        last_button[0] = input()
+
+while True:
+    if last_button[0] == "W":
+        mvm+=1
+    elif last_button[0] == "S":
+        mvm -= 1
+    if last_button[0] != before_last_button[0] and (last_button[0] == "S" or last_button[0] == "W"):
+        if mvm == 1:
+            robot.forward()
+        elif mvm == 0:
+            robot.stop()
+        elif mvm == -1:
+            robot.backward()
+    if last_button[0] == "D":
+        robot.right()
+    elif last_button[0] == "A":
+        robot.left()
