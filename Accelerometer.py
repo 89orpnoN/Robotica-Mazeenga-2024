30fcad456c3d5e4ec371493fcd8b7d4354e778d9
89orpnon <9698022@itisrossi.vi.it>
2024-03-16 01:32:38 +0100

    p

--- Accelerometer.py
+++ Accelerometer.py
@@ -4,20 +4,64 @@ from time import sleep
 
 sensor = mpu6050(0x68)
 
-while True:
-    accel_data = sensor.get_accel_data()
-    gyro_data = sensor.get_gyro_data()
-    temp = sensor.get_temp()
+class  MPU6050:
+    def __init__(self, bus = 1, addr = 0x68):
+        self.Sensor = mpu6050(addr, bus)
+        self.AccelOffset = [0,0,0] #XYZ
+        self.GyroOffset = [0, 0, 0]  # XYZ
+        self.TempOffset = 0
+        self.GyroIgnore = [None,None]
+        self.AccelIgnore = [None,None]
 
-    print("Accelerometer data")
-    print("x: " + str(accel_data['x']))
-    print("y: " + str(accel_data['y']))
-    print("z: " + str(accel_data['z']))
 
-    print("Gyroscope data")
-    print("x: " + str(gyro_data['x']))
-    print("y: " + str(gyro_data['y']))
-    print("z: " + str(gyro_data['z']))
 
-    print("Temp: " + str(temp) + " C")
-    sleep(5)
+
+def Calibrate(mpu,manualtemp = False):
+    if manualtemp:
+        mpu.TempOffset = int(input("offset temperatura: ")) #niente error check, se lo scrivi male sono affari tuoi
+    print("#"*10  + "stabilizzare il robot" + "#"*10)
+    sleep(1)
+    accel = []
+    gyro = []
+    for i in range(1000):
+        print("misurazione: " + str(i))
+
+        accel_data = mpu.Sensor.get_accel_data()
+        accel.append(accel_data)
+
+        gyro_data = mpu.Sensor.get_gyro_data()
+        gyro.append(gyro_data)
+
+
+
+    AccelSamdwich = [accel[0].copy()*3] #sandwich perché è minore, medio e maggiore
+    for i in accel:
+        for j in ["x","y","z"]:
+            if AccelSamdwich[0][j] > i[j]:
+                AccelSamdwich[0][j] = i[j]
+
+            if AccelSamdwich[2][j] < i[j]:
+                AccelSamdwich[2][j] = i[j]
+
+            AccelSamdwich[1][j] += i[j]
+
+    mpu.AccelOffset = AccelSamdwich[1]
+    mpu.AccelIgnore = [AccelSamdwich[0],AccelSamdwich[2]]
+
+    GyroSamdwich = [gyro[0].copy()*3]
+    for i in gyro:
+        for j in ["x", "y", "z"]:
+            if GyroSamdwich[0][j] > i[j]: #min
+                GyroSamdwich[0][j] = i[j]
+
+            if GyroSamdwich[2][j] < i[j]: #max
+                GyroSamdwich[2][j] = i[j]
+
+            GyroSamdwich[1][j] += i[j] #mean
+
+    mpu.GyroOffset = GyroSamdwich[1]
+    mpu.GyroIgnore = [GyroSamdwich[0], GyroSamdwich[2]]
+
+
+mpu = MPU6050()
+Calibrate(mpu)
