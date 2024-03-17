4c5b4fdf91392e41149fac9db89e261244e30469
89orpnon <9698022@itisrossi.vi.it>
2024-03-17 12:03:44 +0100

    inizio della spacial awareness

--- Spacial Awareness.py
+++ Spacial Awareness.py
@@ -7,8 +7,9 @@ gyro_data = None
 current_rotation = numpy.array([0,0,0])
 cycle_T = time.process_time_ns()
 def nm_to_s(int): return int / 1000000
-def deltaT(time):
-    return time.process_time_ns() - time
+def deltaT(l_time):
+    return time.process_time_ns() - l_time
+
 while True:
     gyro_data = numpy.array(am.GetGyroData(am.mpu))
     added_velocity = gyro_data * nm_to_s(deltaT(cycle_T))
